4d15373e06286117ff8a6b46fb6e4eae0409712c
Renato R. Belumat <renato.belumat@alta-rt.com>
2025-02-03 14:53:11 -0300

    first commit

---
+++ minha_biblioteca.cpp
@@ -0,0 +1,6 @@
+#include "minha_biblioteca.h"
+
+int somar(int a, int b)
+{
+    return a + b;
+}
---
+++ minha_biblioteca.h
@@ -0,0 +1,6 @@
+#ifndef MINHA_BIBLIOTECA_H
+#define MINHA_BIBLIOTECA_H
+
+int somar(int a, int b);
+
+#endif
